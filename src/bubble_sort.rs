6cdab9ae8867182c2d6c2ec63fd67825ba6fac3f
tachu <roatyungtatyu@gmail.com>
2024-10-31 14:45:55 +0900

    add: bubble sort

---
+++ src/bubble_sort.rs
@@ -0,0 +1,43 @@
+pub fn bubble_sort(v: &mut Vec<i32>) {
+    let mut swapped = true;
+    while swapped {
+        swapped = false;
+        for i in 0..v.len() - 1 {
+            if v[i] > v[i + 1] {
+                v.swap(i, i + 1);
+                swapped = true;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn crate_actual_and_expect(mut v: Vec<i32>) -> (Vec<i32>, Vec<i32>) {
+        let mut expect = v.clone();
+        bubble_sort(&mut v);
+        expect.sort();
+        (v, expect)
+    }
+
+    #[test]
+    fn small_range() {
+        let (actual, expect) = crate_actual_and_expect(vec![4, 3, 9, 8, 2]);
+        assert_eq!(actual, expect)
+    }
+
+    #[test]
+    fn big_range() {
+        let (actual, expect) = crate_actual_and_expect(vec![-4, 32034243, 239, -32348, 3204823, 0]);
+        assert_eq!(actual, expect);
+    }
+}
+
+fn main() {
+    let mut v: Vec<i32> = vec![3, 1, 9, 0, 4];
+    println!("{:?}", v);
+    bubble_sort(&mut v);
+    println!("{:?}", v);
+}
