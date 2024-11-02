21e9d735a89693804889052f5269237e24adb744
tachu <roatyungtatyu@gmail.com>
2024-11-02 14:47:26 +0900

    add: quick select

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,39 @@
+use std::usize;
+
 pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
-    todo!();
+    let mut clone = nums.clone();
+    let target_index = nums.len() - k as usize;
+    quick_select(&mut clone, 0, nums.len() - 1, target_index)
+}
+
+pub fn quick_select(nums: &mut Vec<i32>, left: usize, right: usize, k: usize) -> i32 {
+    if left == right {
+        return nums[left];
+    }
+    let pivot = partition(nums, left, right);
+    if pivot + 1 == k {
+        nums[k]
+    } else if pivot + 1 > k {
+        quick_select(nums, left, pivot - 1, k)
+    } else {
+        quick_select(nums, pivot + 1, right, k)
+    }
+}
+
+pub fn partition(nums: &mut Vec<i32>, left: usize, right: usize) -> usize {
+    let pivot = nums[right];
+    let mut small_index = left;
+    println!("pivot={},pi={} si={}", pivot, right, small_index);
+    for i in left..right {
+        if pivot > nums[i] {
+            nums.swap(i, small_index);
+            small_index += 1;
+        }
+    }
+    println!("pi={} si={}", small_index, right);
+    nums.swap(small_index, right);
+    println!("si={} ", small_index);
+    small_index
 }
 
 #[cfg(test)]
@@ -8,25 +42,25 @@ mod utils_test {
 
     #[test]
     fn fkl_small_range() {
-        assert_eq!(find_kth_largest(vec![4, 3, 9, 8, 2], 4), 3);
+        assert_eq!(find_kth_largest(vec![4, 3, 9, 8, 2], 3), 3);
     }
 
     #[test]
     fn fkl_big_range() {
         assert_eq!(
-            find_kth_largest(vec![-4, 32034243, 239, -32348, 3204823, 0], 2),
-            3204823
+            find_kth_largest(vec![-4, 10000, 239, -32348, 1000, 1], 2),
+            1000
         );
     }
 
     #[test]
     fn fkl_duplicate_nums() {
-        assert_eq!(find_kth_largest(vec![2, -1, 0, 7, 2, 7], 0), -1);
+        assert_eq!(find_kth_largest(vec![2, -1, 0, 7, 2, 7], 1), 7);
     }
 }
 
 fn main() {
-    let mut v: Vec<i32> = vec![3, 1, 9, 0, 4];
+    let v: Vec<i32> = vec![4, 3, 9, 8, 2];
     println!("{:?}", v);
-    println!("{}", find_kth_largest(v, 0));
+    println!("{}", find_kth_largest(v, 3));
 }
